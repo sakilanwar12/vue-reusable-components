53750f3ff9d8c92a7def1ba9e18271b1b509f4b1
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2023-10-25 23:29:24 +0600

    vue:init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<!-- https://www.youtube.com/watch?v=_CdLj6ExjX0&list=PLC3y8-rFHvwgeQIfSDtEGVvvSEPDkL_1f&index=8 -->
+
+<template lang="">
+  <div class="container mx-auto">
+      <h2 v-for="(name,index) in names" :key="name">Name: {{name}}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      names: ["sakil", "hasin", "sumit"]
+    }
+  }
+}
+</script>
