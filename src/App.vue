049c4de6e24e446a65d4805586623914e086e226
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2023-10-25 23:54:53 +0600

    button added

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <!-- https://www.youtube.com/watch?v=_CdLj6ExjX0&list=PLC3y8-rFHvwgeQIfSDtEGVvvSEPDkL_1f&index=8 -->
 
-<template lang="">
-  <div class="container mx-auto">
-      <h2 v-for="(name,index) in names" :key="name">Name: {{name}}</h2>
-  </div>
-</template>
-
 <script>
-
+import Button from './ui/button/Button.vue';
 export default {
-  data() {
-    return {
-      names: ["sakil", "hasin", "sumit"]
-    }
+  components: {
+    appbutton: Button
   }
 }
 </script>
+
+<template>
+  <div class="container mx-auto py-10">
+    <div class="space-x-5">
+      <appbutton>primary </appbutton>
+      <appbutton>secondary</appbutton>
+      <appbutton>Warning</appbutton>
+      <appbutton>info</appbutton>
+      <appbutton>success</appbutton>
+    </div>
+
+  </div>
+</template>
---
+++ src/ui/button/Button.vue
@@ -0,0 +1,5 @@
+
+
+<template lang="">
+    <button type="button" class="h-10 px-5 bg-gray-900 text-white rounded-sm text-lg font-medium capitalize"><slot/></button>
+</template>
